910211ef3febcf5a6502192980a83e34cd89f8b7
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-08 16:57:59 -0400

    Create s2.java

---
+++ 771. Jewels and Stones/s2.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int result = 0;
+        for(char ch: J.toCharArray()){
+            map.put(ch, 1);
+        }
+        for(char ch: S.toCharArray()){
+            if(map.containsKey(ch))
+                ++result;
+        }
+        return result;
+    }
+}
+// hashmap
+// O(J+S) two loops
+// O(J+S)
