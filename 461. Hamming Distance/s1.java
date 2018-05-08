9c4896e578b4c0523e1bc39493c554676c63aa5b
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-08 17:35:11 -0400

    Create s1.java

---
+++ 461. Hamming Distance/s1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int xor = x ^ y, count = 0;
+        for(int i = 0; i < 32; ++i){
+            count += (xor >> i) & 1;
+        }
+        return count;
+    }
+}
+// bit manipulation
+// O(n) len of the number
+// O(n)
