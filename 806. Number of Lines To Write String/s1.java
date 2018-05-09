4bd72f2378f42034106e089d6c8a91b72a6c4d83
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-09 16:59:47 -0400

    Create s1.java

---
+++ 806. Number of Lines To Write String/s1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] numberOfLines(int[] widths, String S) {
+        int line = 1, offset = 0;
+        for(char c: S.toCharArray()){
+            int w = widths[c - 'a'];
+            offset += w;
+            if(offset > 100){
+                line++;
+                offset = w;
+            }
+        }
+        return new int[]{line, offset};
+    }
+}
+// O(n) string length
+// O(1)
