8a685f1ca20058539eef88a79fc75aae9f45bd71
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-10 16:34:56 -0400

    Create s1.java

---
+++ 575. Distribute Candies/s1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int distributeCandies(int[] candies) {
+        // first loop over see how many kinds
+        // sister get all kinds, if not in order
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < candies.length; ++i){
+            if(map.get(candies[i]) != null){
+                map.put(candies[i], map.get(candies[i]) + 1);
+            }else
+                map.put(candies[i], 1);
+        }
+        int index = map.keySet().size();
+        if(index >= candies.length / 2)
+            return candies.length / 2;
+        else
+            return index;
+    }
+}
+// O(n)
+// O(n)
