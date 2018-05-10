65911cda491d0308c4b69b386dda64283602cbda
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-10 17:38:53 -0400

    Create s1.java

---
+++ 669. Trim a Binary Search Tree/s1.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int L, int R) {
+        if(root == null) return root;
+        
+        if(root.val < L) return trimBST(root.right, L, R);
+        if(root.val > R) return trimBST(root.left, L, R);
+        
+        root.left = trimBST(root.left, L, R);        
+        root.right = trimBST(root.right, L, R);
+        return root;
+    }
+}
+// first check the edge condition
+// if < L, the result tree should be right of the node
+// if > R, the result tree should be left of the node
+// O(n) number of nodes, visit each node at once
+// O(n) stack for nodes 
