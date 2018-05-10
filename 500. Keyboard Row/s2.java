24bad54dca4d068908c673b1fe06739431417318
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-10 13:39:13 -0400

    Create s2.java

---
+++ 500. Keyboard Row/s2.java
@@ -0,0 +1,6 @@
+// ???
+// Java 1-Line Solution via Regex and Stream
+public String[] findWords(String[] words) {
+    return Stream.of(words).filter(s -> s.toLowerCase().matches("[qwertyuiop]*|[asdfghjkl]*|[zxcvbnm]*")).toArray(String[]::new);
+}
+// not sure about this
