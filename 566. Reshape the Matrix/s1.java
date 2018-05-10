a94e7a136c6019af5d71c4a851f56ea66b59a181
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-10 18:29:34 -0400

    Create s1.java

---
+++ 566. Reshape the Matrix/s1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int row = nums.length;
+        int col = nums[0].length;
+        if(r * c != row * col) return nums;
+        
+        int[][] newnum = new int[r][c];
+        for(int i = 0; i < r; ++i){
+            for(int j = 0; j < c; ++j){
+                newnum[i][j] = nums[(i*c+j)/col][(i*c+j)%col];
+            }
+        }
+        return newnum;
+    }
+}
+// O(mn)
+// O(mn)
