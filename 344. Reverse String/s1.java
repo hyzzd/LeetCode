e73092d338bda27ad32cc1cf554fb95f23027f2a
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-10 09:14:28 -0400

    Create s1.java

---
+++ 344. Reverse String/s1.java
@@ -0,0 +1,8 @@
+public String reverseString(String s) {
+    StringBuilder sb = new StringBuilder(s);
+    return sb.reverse().toString();
+}
+// using the buildin method
+// String class does not have reverse() 
+// O(n)
+// O(n)
