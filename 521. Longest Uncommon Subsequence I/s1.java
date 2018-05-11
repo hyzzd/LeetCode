23a87fd72aee884dc671aabe046311b07bae5683
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-11 09:08:22 -0400

    Create s1.java

---
+++ 521. Longest Uncommon Subsequence I/s1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if(a.equals(b))
+            return -1;
+        return Math.max(a.length(), b.length());
+    }
+}
+// this problem has an issue that
+// abcabc does not contain subseq with abcabd
+// O(min(a, b)) equals method
+// O(1)
