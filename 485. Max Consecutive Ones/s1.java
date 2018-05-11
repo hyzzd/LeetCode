d35389b7700b91846c5743488a7d8a5384c4f204
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-11 11:48:00 -0400

    Update s1.java

--- 485. Max Consecutive Ones/s1.java
+++ 485. Max Consecutive Ones/s1.java
@@ -1,1 +1,11 @@
-
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, count = 0;
+        for(int i = 0; i < nums.length; ++i){
+            max = Math.max(max, count = nums[i] == 0 ? 0 : count + 1);
+        }
+        return max;
+    }
+}
+// O(n)
+// O(1)
