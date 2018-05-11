43d378e022d5b73d643b9353d6cc5c1f24d56f1d
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-11 14:02:29 -0400

    Create s1.java

---
+++ 226. Invert Binary Tree/s1.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) { //invert the nodes left and right
+        if(root == null) return null;
+    
+        TreeNode left = invertTree(root.right);
+        TreeNode right = invertTree(root.left);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
+// recursive
+// O(n)
+// O(h) function calls will be placed on the stack, h is the height of tree
