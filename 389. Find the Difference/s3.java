2cedca77d817758ce6ea9bee4f4b66ef0a4c2351
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-14 13:17:03 -0400

    Create s3.java

---
+++ 389. Find the Difference/s3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int ch = t.charAt(t.length() - 1);
+        for(int i = 0; i < s.length(); ++i){
+            ch ^= t.charAt(i);
+            ch ^= s.charAt(i);
+        }
+        return (char) ch;
+    }
+}
+// similar as s2
