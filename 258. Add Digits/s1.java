9fadfed772644169e242286b6977b8087874738d
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-14 10:22:27 -0400

    Create s1.java

---
+++ 258. Add Digits/s1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num < 10 && num >= 0) return num;
+        
+        int i = 0;
+        while(num > 0){
+            i += num % 10;
+            num /= 10;
+        }
+        return addDigits(i);
+    }
+}
+// O(n)
+// O(1)
