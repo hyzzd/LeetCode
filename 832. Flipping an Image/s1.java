110c0c5eb1357f20a34bf4f9a0e09561b8bb674f
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-14 12:51:33 -0400

    Create s1.java

---
+++ 832. Flipping an Image/s1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        
+        for(int i = 0; i < A.length; ++i){
+            for(int l = 0, r = A[0].length - 1; l < r; ++l, --r){
+                int temp = A[i][l];;
+                A[i][l] = A[i][r];
+                A[i][r] = temp;
+            }
+            for(int j = 0; j < A[0].length; ++j){
+                A[i][j] ^= 1;
+            }
+        }
+        return A;
+    }
+}
