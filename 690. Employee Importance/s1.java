d5445c1d326438ae01f7e34ab261368dc57c926d
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-15 14:36:39 -0400

    Create s1.java

---
+++ 690. Employee Importance/s1.java
@@ -0,0 +1,31 @@
+/*
+// Employee info
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
+*/
+class Solution {
+    Map<Integer, Employee> map = new HashMap<>();
+    
+    public int getImportance(List<Employee> employees, int id) {
+        if(employees == null) return 0;
+        //Map<Integer, Employee> map = new HashMap<>();
+        for(Employee e: employees) map.put(e.id, e);
+        return dfs(id);
+    }
+    
+    public int dfs(int eid){
+        Employee employee = map.get(eid);
+        int ans = employee.importance;
+        for(Integer subid: employee.subordinates){
+            ans += dfs(subid);
+        }        
+        return ans;
+    }
+}
