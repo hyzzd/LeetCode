b2c91d9b3e78babf452e9f36f090a1301743ff25
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-15 15:19:37 -0400

    Create s1.java

---
+++ 448. Find All Numbers Disappeared in an Array/s1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new LinkedList<>();
+        for(int i = 0; i < nums.length; ++i) nums[(nums[i] - 1) % nums.length] += nums.length;
+        for(int i = 0; i < nums.length; ++i){
+            if(nums[i] <= nums.length)
+                list.add(i + 1);
+        }
+        return list;
+    }
+}
+// O(n)
+// O(n)
