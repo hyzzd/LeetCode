2214d41cf9df434c4c42c94ff4590bc323d4c0ca
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-16 09:05:12 -0400

    Create s1.java

---
+++ 830. Positions of Large Groups/s1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String S) {
+        //S = "aaa";
+        List<List<Integer>> ans = new LinkedList<>();
+        if(S.length() < 3) return ans;
+        int j = 0;
+        for(int i = 0; i < S.length(); ++i){
+            if(i == S.length()-1 || S.charAt(i) != S.charAt(i+1)){
+                if(i-j+1 >= 3){
+                    ans.add(Arrays.asList(new Integer[]{j, i}));
+                }
+                j = i+1;
+            }
+        }
+        
+        return ans;
+    }
+}
+// O(n)
+// O(n)
