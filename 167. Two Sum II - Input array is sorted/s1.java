ee28a1d7f2e74aa06597507c2e6b87ea7322068e
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-18 16:11:24 -0400

    Create s1.java

---
+++ 167. Two Sum II - Input array is sorted/s1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+        int l = 0, r = numbers.length - 1;
+        while(l < r){
+            if(numbers[l]+numbers[r]<target) l++;
+            else if(numbers[l]+numbers[r]>target) r--;
+            else{
+                ans[0] = l+1;
+                ans[1] = r+1;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+// O(n)
+// O(1)
