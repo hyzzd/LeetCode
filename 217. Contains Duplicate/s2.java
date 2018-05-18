f0c2e8ffe03903af59fa8be3adf50545bf07ed27
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-18 14:38:20 -0400

    Create s2.java

---
+++ 217. Contains Duplicate/s2.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; ++i){
+            if(map.getOrDefault(nums[i], 0) != 0)
+                return true;
+            else
+                map.put(nums[i], 1);
+        }
+        return false;
+    }
+}
+// O(n)
+// O(n)
