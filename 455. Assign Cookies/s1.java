6174ec99b1486ccaaeadf53e50a3d964e6de807f
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-18 15:02:22 -0400

    Create s1.java

---
+++ 455. Assign Cookies/s1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int j = 0;
+        for(int i = 0; i < s.length && j < g.length; ++i){
+            if(g[j] <= s[i]) {j++;}
+        }
+        return j;
+    }
+}
+// O(nlgn)
+// O(1)
