fbe3ba1b0c35cc57ff71c961dce2118089b49e4c
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-19 21:20:30 -0400

    Create s1.java

---
+++ 563. Binary Tree Tilt/s1.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    
+    public int findTilt(TreeNode root) {
+        helper(root);
+        return ans;
+    }
+    
+    private int helper(TreeNode root){ // returns the sum of subtree
+        if(root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        ans += Math.abs(left - right);
+        return left+right+root.val;
+    }
+}
+// O(n)
+// O(h)
