4ff8e15df654d96478b03187415d8e556e70fc47
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-20 21:36:15 -0400

    Create s3.java

---
+++ 268. Missing Number/s3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        long ans = 0;
+        for(long i : nums) ans ^= ((long) 1 << i);
+        int index = 0;
+        while(ans > 0){
+            if(ans % 2 == 0){
+                break;
+            }
+            ans /= 2;
+            index++;
+        }
+        return index;
+    }
+}
+// This method limits by the long is 2^64. Thus the nums arrays should be shorter than 64.
