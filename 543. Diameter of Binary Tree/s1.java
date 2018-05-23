98278d43db76ca80a8d25a608dfd340cee45a175
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-22 22:03:16 -0400

    Create s1.java

---
+++ 543. Diameter of Binary Tree/s1.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans;
+    public int diameterOfBinaryTree(TreeNode root) {
+        ans = 1;
+        dfs(root);
+        return ans - 1;
+    }
+    
+    public int dfs(TreeNode root){
+        if(root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        ans = Math.max(ans, left+right+1);
+        return Math.max(left, right) + 1;
+    }
+}
+// O(n)
+// O(h)
