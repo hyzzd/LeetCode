00236901fbdd20ca8e520ed9b4ad47910eb65504
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-23 10:14:51 -0400

    Create s2.java

---
+++ 504. Base 7/s2.java
@@ -0,0 +1,8 @@
+public String convertTo7(int num) {
+    if (num < 0)
+        return '-' + convertTo7(-num);
+    if (num < 7)
+        return num + "";
+    return convertTo7(num / 7) + num % 7;
+}
+// Recursive
