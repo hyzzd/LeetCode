822c586b3b3cda163e3b21c220361d9ae0d7d6b6
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-23 10:53:37 -0400

    Create s2.java

---
+++ 744. Find Smallest Letter Greater Than Target/s2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left = 0, right = letters.length, mid;
+        while(left < right){
+            mid = (left + right) / 2;
+            if(letters[mid] > target) right = mid;
+            else left = mid + 1;
+        }
+        return letters[left % letters.length];
+    }
+}
+// O(lgn)
+// O(1)
