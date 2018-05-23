188873f1db2f1e7689955e423dd7d0bdd149461c
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-23 09:12:28 -0400

    Create s4.java

---
+++ 350. Intersection of Two Arrays II/s4.java
@@ -0,0 +1,2 @@
+// If nums2 doesn't fit to the memory, store nums1 in a HashMap and read trunks of num2 that fits into the memory and record the intersections
+// If nums1 and nums2 both don't fit, sort them individually and read 2 elements into the memory and record intersection
