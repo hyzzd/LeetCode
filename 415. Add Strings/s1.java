29253a7e0bd6035087e9af397340c42afe6196e0
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-24 15:35:55 -0400

    Create s1.java

---
+++ 415. Add Strings/s1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        char[] num1Array = num1.toCharArray();
+        char[] num2Array = num2.toCharArray();
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(carry == 1 || i >= 0 || j >= 0){
+            int a = i >= 0 ? (num1Array[i--] - '0') : 0;
+            int b = j >= 0 ? (num2Array[j--] - '0') : 0;
+            int sum = a + b + carry;
+            sb.insert(0, sum % 10);
+            carry = sum / 10;
+        }
+        return sb.toString();
+    }
+}
+// O(m+n)
+// O(m+n)
