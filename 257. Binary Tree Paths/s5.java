223e228752b7abce951e527a452d11b62a10fa5c
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-24 17:00:55 -0400

    Update s5.java

--- 257. Binary Tree Paths/s5.java
+++ 257. Binary Tree Paths/s5.java
@@ -1,1 +1,34 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> list = new LinkedList<>();
+        Stack<TreeNode> sNode = new Stack<>();
+        Stack<String> sStr = new Stack<>();
+        
+        if(root == null) return list;
+        sNode.push(root);
+        sStr.push("");
+        while(!sNode.isEmpty()){
+            TreeNode curNode = sNode.pop();
+            String curStr = sStr.pop();
+            if(curNode.left == null && curNode.right == null) list.add(curStr+curNode.val);
+            if(curNode.left != null){
+                sNode.push(curNode.left);
+                sStr.push(curStr+curNode.val+"->");
+            }
+            if(curNode.right != null){
+                sNode.push(curNode.right);
+                sStr.push(curStr+curNode.val+"->");
+            }
+        }
+        return list;
+    }
+}
