6210fe729134ec96e6c3037c24b48c4fc5afa428
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-24 22:18:20 -0400

    Create s1.java

---
+++ 70. Climbing Stairs/s1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        return dfs(0, n);
+    }
+    public int dfs(int i, int n){
+        if(i > n){
+            return 0;
+        }
+        if(i == n){
+            return 1;
+        }
+        return dfs(i + 1, n) + dfs(i + 2, n);
+    }
+}
+// O(2^n)
+// O(n) depth
