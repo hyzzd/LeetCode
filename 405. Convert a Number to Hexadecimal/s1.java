0e738649bccd6701f30e710ab48373a72a11c6a4
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-25 11:08:42 -0400

    Create s1.java

---
+++ 405. Convert a Number to Hexadecimal/s1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String toHex(int num) {
+        if(num == 0) return String.valueOf(num);
+        StringBuilder sb = new StringBuilder();
+        
+        while(num != 0){
+            int digit = num & 0xf;
+            sb.append(digit<10?(char)(digit+'0'):(char)(digit-10+'a'));
+            num >>>= 4;
+        }
+        
+        return sb.reverse().toString();
+    }
+}
