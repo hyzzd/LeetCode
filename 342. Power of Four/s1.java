f953d4c3c93d1f1f0c2adc42dc6f5fd747e12e81
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-26 10:58:35 -0400

    Create s1.java

---
+++ 342. Power of Four/s1.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean isPowerOfFour(int num) {
+        if (num == 0) return false;
+        while (num != 0){
+            if (num == 1) return true;
+            if (num % 4 != 0){
+                System.out.print(num);
+                return false;
+            }
+            else{
+                num = num / 4;
+            }
+        }
+        return true;
+    }
+}
+
+/*
+public class Solution {
+public boolean isPowerOfFour(int num) {
+    if(num <= 0)
+        return false;
+    return ((num - 1) & num) == 0 && (num & 0x55555555) != 0; // 注意 & 的优先级比 == 低
+}
+*/
