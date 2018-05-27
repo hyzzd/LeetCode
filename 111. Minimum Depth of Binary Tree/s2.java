909293de112acf9715f42889f23aa0fc0c197658
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-27 16:11:50 -0400

    Create s2.java

---
+++ 111. Minimum Depth of Binary Tree/s2.java
@@ -0,0 +1,21 @@
+public int minDepth(TreeNode root) {
+    if(root == null) return 0;
+    Queue<TreeNode> que = new LinkedList();
+    int level =1;
+    que.add(root);
+    while(!que.isEmpty()){
+        int size = que.size();
+        while(size>0){
+            TreeNode node =que.poll();
+            if(node.left == null && node.right ==null)
+                return level;
+            if(node.left != null)
+                que.add(node.left);
+            if(node.right != null)
+                que.add(node.right);
+            size--;
+        }
+        level++;
+    }
+    return level;
+}
