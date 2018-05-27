e54d57f24404bc6bb6edb55b7b9b3e23639b0c94
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-27 10:19:55 -0400

    Create s1.java

---
+++ 9. Palindrome Number/s1.java
@@ -0,0 +1,11 @@
+public class Solution {
+    public boolean isPalindrome(int x) {  // Cannot solve it without using extra space
+        int sum = 0, target = x;
+        while (x > 0) {
+            int temp = x % 10;
+            x /= 10;
+            sum = sum * 10 + temp;
+        }
+        return sum == target;
+    }
+}
