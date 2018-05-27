35ab1b8ac1a2475a0d87bf080a7871d2060fbfbb
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-27 15:36:49 -0400

    Create s1.java

---
+++ 20. Valid Parentheses/s1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() == 0)
+            return false;
+        Stack<Character> stack = new Stack<Character>();
+        for(char c : s.toCharArray()){
+            if(c == '(')
+                stack.push(')');
+            else if(c == '[')
+                stack.push(']');
+            else if(c == '{')
+                stack.push('}');
+            else if(stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
