b73b7ce35450205d0db8bce3d9c9038de02da6a4
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-27 16:48:29 -0400

    Create s1.java

---
+++ 234. Palindrome Linked List/s1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> list = new LinkedList<>();
+        if(s == null||s.length() == 0||p == null||p.length() == 0) return list;
+        int[] hash = new int[128];
+        for(char c: p.toCharArray()){
+            hash[c]++;
+        }
+        int left = 0, right = 0, count = p.length();
+        while(right < s.length()){
+            if(hash[s.charAt(right++)]-- >= 1) count--;
+            if(count == 0) list.add(left);
+            if(right - left == p.length() && hash[s.charAt(left++)]++ >= 0) count++;
+        }
+        return list;
+    }
+}
+// O(n)
