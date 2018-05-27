f1cbd2364a4d50e08503ed4a32da80348f0bc668
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-27 09:32:43 -0400

    Create s1.java

---
+++ 434. Number of Segments in a String/s1.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countSegments(String s) {
+        String str = s.trim();
+        if(str.length() == 0) return 0;
+        return str.split("\\s+").length;
+    }
+}
+// O(n)
+// O(n)
