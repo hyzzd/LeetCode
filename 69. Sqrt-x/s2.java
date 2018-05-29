d39261cbb7eeb777f7b25ebe73e54d2ef2237f2e
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-29 17:56:39 -0400

    Create s2.java

---
+++ 69. Sqrt-x/s2.java
@@ -0,0 +1,17 @@
+// binary search
+class Solution {
+    public int mySqrt(int x) {
+        if(x == 0)
+            return 0;
+        int left = 0; right = Integer.MAX_VALUE;
+        while(true){
+            int mid = (left + right) / 2;
+            if(mid * mid > x) right = mid - 1;
+            else{
+                if((mid + 1) * (mid + 1) > x)
+                    return mid;
+                left = mid + 1;
+            }
+        }
+    }
+}
