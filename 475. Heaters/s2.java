70449b6e0f512aa3471a710dcd06bdabc5fe43b8
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-29 16:24:47 -0400

    Create s2.java

---
+++ 475. Heaters/s2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        Arrays.sort(houses);
+        
+        int i = 0, res = 0;
+        for(int house:houses){
+            while(i < heaters.length - 1 && heaters[i] + heaters[i + 1] <= 2*house) i++;
+            res = Math.max(res, Math.abs(heaters[i] - house));
+        }
+        return res;
+    }
+}
