0cb2989dcead12a1a3ef2cb0ee06919243bcb85e
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-29 11:21:45 -0400

    Create s1.java

---
+++ 290. Word Pattern/s1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split("\\s+");
+        if(pattern.length() != words.length) return false;
+        Map<Character, String> map = new HashMap<>();
+        for(int i = 0; i < words.length; ++i){
+            char c = pattern.charAt(i);
+            if(map.containsKey(c)){
+                if(!map.get(c).equals(words[i])) return false;
+            }else{
+                if(map.containsValue(words[i])) return false;
+                map.put(c, words[i]);
+            }
+        }
+        return true;
+    }
+}
+// O(n)
+// O(n)
