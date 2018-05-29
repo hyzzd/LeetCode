5dad509c8b1cdb32c7da3708187d9e2a3b769168
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-29 11:49:25 -0400

    Update s1.java

--- 88. Merge Sorted Array/s1.java
+++ 88. Merge Sorted Array/s1.java
@@ -6,16 +6,16 @@ class Solution {
 
         while(i >= 1 && j >= 1){
             if(nums1[i - 1] >= nums2[j - 1]){
-                nums1[i + j - 1] = nums1[i];
+                nums1[i + j - 1] = nums1[i - 1];
                 i--;
             }
             else{
-                nums1[i + j - 1] = nums2[j];
+                nums1[i + j - 1] = nums2[j - 1];
                 j--;
             }
         }
         while(j >= 1){
-            nums1[i + j - 1] = nums2[j];
+            nums1[i + j - 1] = nums2[j - 1];
             j--;
         }            
     }
