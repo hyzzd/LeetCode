05a0601da4869bf2ad403365bd1165c79bdc1b88
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-30 09:55:58 -0400

    Create s1.java

---
+++ 807. Max Increase to Keep City Skyline/s1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxIncreaseKeepingSkyline(int[][] grid) {
+        int N = grid.length;
+        int[] rowMax = new int[N];
+        int[] colMax = new int[N];
+        
+        for(int r = 0; r < N; ++r){
+            for(int c = 0; c < N; ++c){
+                rowMax[r] = Math.max(rowMax[r], grid[r][c]);
+                colMax[c] = Math.max(colMax[c], grid[r][c]);
+            }
+        }
+        int ans = 0;
+        for(int r = 0; r < N; ++r){
+            for(int c = 0; c < N; ++c){
+                ans += Math.min(rowMax[r], colMax[c]) - grid[r][c];
+            }
+        }
+        return ans;
+    }
+}
+// O(N^2)
+// O(N)
