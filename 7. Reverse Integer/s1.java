1e9b600395fb8dc8c56bea437c710439d257e52b
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-29 23:25:10 -0400

    Create s1.java

---
+++ 7. Reverse Integer/s1.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int reverse(int x) {
+        long sum = 0;
+        while (x != 0) {
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+        }
+        return (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE ? 0 : (int) sum); //Notice about edge
+    }
+}
