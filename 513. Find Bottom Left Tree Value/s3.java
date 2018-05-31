a85f529cc11a5877cbbb02b4850b5d28c1bd7166
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-31 11:21:19 -0400

    Create s3.java

---
+++ 513. Find Bottom Left Tree Value/s3.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans = 0, h = 0;
+    public int findBottomLeftValue(TreeNode root) {
+        findBottomLeftValue(root, 1);
+        return ans;
+    }
+    public void findBottomLeftValue(TreeNode root, int depth){
+        if(h < depth) {
+            ans = root.val;
+            h = depth;
+        }
+        if(root.left != null) findBottomLeftValue(root.left, depth+1);
+        if(root.right != null) findBottomLeftValue(root.right, depth+1);
+    }
+}
+// Overloading
