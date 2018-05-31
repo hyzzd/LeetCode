992604c0562c5b60a435caac42a9d4efc4c8c86c
hyzzd <zhaoxiaohu920922@gmail.com>
2018-05-31 12:46:14 -0400

    Create s1.java

---
+++ 540. Single Element in a Sorted Array/s1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        while(l < r){
+            int mid = l + (r - l) / 2;
+            if(mid % 2 == 1) mid--;
+            if(nums[mid] != nums[mid + 1]) r = mid;
+            else l = mid + 2;
+        }
+        return nums[l];
+    }
+}
