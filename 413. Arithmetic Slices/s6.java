fc8bb99fb595e34c29fe0e28b990c60ceacd047b
hyzzd <zhaoxiaohu920922@gmail.com>
2018-06-04 22:07:13 -0400

    Create s6.java

---
+++ 413. Arithmetic Slices/s6.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int numberOfArithmeticSlices(int[] A) {
+        int count = 0;
+        int sum = 0;
+        for (int i = 2; i < A.length; i++) {
+            if (A[i] - A[i - 1] == A[i - 1] - A[i - 2]) {
+                count++;
+            } else {
+                sum += (count + 1) * (count) / 2;
+                count = 0;
+            }
+        }
+        return sum += count * (count + 1) / 2;
+    }
+}
+// O(n)
+// O(1)
