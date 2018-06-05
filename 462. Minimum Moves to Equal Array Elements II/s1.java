83bb5066f8980f3ffbdd68e8009cf7595c0a5160
hyzzd <zhaoxiaohu920922@gmail.com>
2018-06-05 17:37:50 -0400

    Create s1.java

---
+++ 462. Minimum Moves to Equal Array Elements II/s1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0, j = nums.length - 1;
+        int count = 0;
+        while(i < j){
+            count += nums[j--] - nums[i++];
+        }
+        return count;
+    }
+}
+// Nice solution
+// O(nlgn)
+// O(1)
