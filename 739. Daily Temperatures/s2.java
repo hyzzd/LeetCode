73c7953d901733ea6c63aaef4cef89bc72876715
hyzzd <zhaoxiaohu920922@gmail.com>
2018-06-05 11:47:09 -0400

    Create s2.java

---
+++ 739. Daily Temperatures/s2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+        int[] next = new int[101];
+        Arrays.fill(next, Integer.MAX_VALUE);
+        for(int i = temperatures.length - 1; i >= 0; --i){
+            int warmer_index = Integer.MAX_VALUE;
+            for(int t = temperatures[i] + 1; t <= 100; ++t){
+                if(next[t] < warmer_index)
+                    warmer_index = next[t];   
+            }
+            if(warmer_index < Integer.MAX_VALUE)
+                ans[i] = warmer_index - i;
+            next[temperatures[i]] = i;
+        }
+        return ans;
+    }
+}
+// O(n)
+// O(n+w)
