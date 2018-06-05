ffe49d5aa0e3e57d46a6356231c3b953518e6e5b
hyzzd <zhaoxiaohu920922@gmail.com>
2018-06-04 22:47:18 -0400

    Create s1.java

---
+++ 260. Single Number III/s1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int diff = 0;
+        for(int num : nums){
+            diff ^= num;
+        }
+        diff &= -diff;
+        int[] res = new int[]{0, 0};
+        for(int num : nums){
+            if((num & diff) == 0) res[0] ^= num;
+            else res[1] ^= num;
+        }
+        return res;
+    }
+}
+// O(n)
+// O(1)
