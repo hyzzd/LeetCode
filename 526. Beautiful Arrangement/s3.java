2428ec296046f450a41022148e5a7ddbf29afd31
hyzzd <zhaoxiaohu920922@gmail.com>
2018-06-05 11:18:12 -0400

    Create s3.java

---
+++ 526. Beautiful Arrangement/s3.java
@@ -0,0 +1,20 @@
+class Solution {
+    int count = 0;
+    public int countArrangement(int N) {
+        boolean[] visited = new boolean[N+1];
+        calculate(visited, N, 1);
+        return count;
+    }
+    public void calculate(boolean[] visited, int N, int l){
+        if(l > N) count++;
+        for(int i = 1; i <= N; ++i){
+            if(!visited[i] && (l % i == 0 || i % l == 0)){
+                visited[i] = true;
+                calculate(visited, N, l + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
+// O(number of valid permutation)
+// O(n)
