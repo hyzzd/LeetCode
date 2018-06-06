2daf8bce4ded9ab29a02df903f58b9cd1e3f2263
hyzzd <zhaoxiaohu920922@gmail.com>
2018-06-06 18:00:04 -0400

    Create s1.java

---
+++ 817. Linked List Components/s1.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> set = new HashSet();
+        for(int i:G) set.add(i);
+        ListNode cur = head;
+        int ans = 0;
+        while(cur != null){
+            if(set.contains(cur.val) && (cur.next == null || !set.contains(cur.next.val)))
+                ans++;
+            cur = cur.next;
+        }
+        return ans;
+    }
+}
+// O(n)
+// O(n)
