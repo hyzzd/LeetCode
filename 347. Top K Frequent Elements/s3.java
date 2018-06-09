0950bab608ea952e88b5d53322938dddbf2e12d6
hyzzd <zhaoxiaohu920922@gmail.com>
2018-06-09 16:06:02 -0400

    Create s3.java

---
+++ 347. Top K Frequent Elements/s3.java
@@ -0,0 +1,25 @@
+// use treeMap. Use freqncy as the key so we can get all freqencies in order
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int n: nums){
+            map.put(n, map.getOrDefault(n,0)+1);
+        }
+        
+        TreeMap<Integer, List<Integer>> freqMap = new TreeMap<>();
+        for(int num : map.keySet()){
+           int freq = map.get(num);
+           if(!freqMap.containsKey(freq)){
+               freqMap.put(freq, new LinkedList<>());
+           }
+           freqMap.get(freq).add(num);
+        }
+        
+        List<Integer> res = new ArrayList<>();
+        while(res.size()<k){
+            Map.Entry<Integer, List<Integer>> entry = freqMap.pollLastEntry();
+            res.addAll(entry.getValue());
+        }
+        return res;
+    }
+}
