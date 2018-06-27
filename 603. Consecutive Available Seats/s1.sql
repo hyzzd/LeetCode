333fbbacc4b8716b7abac21a1e596f2fdf1f43bf
hyzzd <zhaoxiaohu920922@gmail.com>
2018-06-27 03:33:13 -0400

    Create s1.sql

---
+++ 603. Consecutive Available Seats/s1.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT DISTINCT c1.seat_id
+FROM cinema AS c1
+JOIN cinema AS c2
+ON ABS(c1.seat_id - c2.seat_id) = 1 AND c1.free = 1 AND c2.free = 1
+ORDER BY c1.seat_id;
