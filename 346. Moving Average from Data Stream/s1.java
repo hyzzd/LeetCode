a4530b22ddf3ea0ba1451a0817839649020aa10c
hyzzd <zhaoxiaohu920922@gmail.com>
2018-07-14 09:36:58 -0400

    Create s1.java

---
+++ 346. Moving Average from Data Stream/s1.java
@@ -0,0 +1,27 @@
+class MovingAverage {
+
+    Queue<Integer> q;
+    int s;
+    double sum;
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        q = new LinkedList<>();
+        s = size;
+        sum = 0;
+    }
+    
+    public double next(int val) {
+        q.offer(val);
+        sum += (double) val;
+        if(q.size() > s){
+            sum -= (double) q.poll();
+        }        
+        return sum / (double) q.size();
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
