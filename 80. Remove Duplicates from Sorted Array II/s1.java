bf6dc25434d5bb4fb084a57b036aedb056f638de
hyzzd <zhaoxiaohu920922@gmail.com>
2018-07-24 08:55:54 -0400

    Create s1.java

---
+++ 80. Remove Duplicates from Sorted Array II/s1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length < 3) return nums.length;
+        int i = 0;
+        for(int j = 2;j < nums.length;j++)
+        {
+            if(nums[j] != nums[i])
+            {
+                i++;
+                nums[i+1] = nums[j];
+            }
+        }
+        return i+2;
+    }
+}
