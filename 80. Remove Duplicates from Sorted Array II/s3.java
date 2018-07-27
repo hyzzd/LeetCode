5ce10835ba6de2e4bedab7659fa5e678a30c009e
hyzzd <zhaoxiaohu920922@gmail.com>
2018-07-27 08:39:59 -0400

    Create s3.java

---
+++ 80. Remove Duplicates from Sorted Array II/s3.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int index = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i > 0 && i < n - 1 && nums[i] == nums[i - 1] && nums[i] == nums[i + 1])
+                continue;
+
+            nums[index++] = nums[i];
+        }
+        return index;
+    }
+};
