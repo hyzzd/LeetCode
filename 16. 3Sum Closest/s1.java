5544a3c97c70e81947e678679f7a0a2920fd84b3
hyzzd <zhaoxiaohu920922@gmail.com>
2018-07-30 14:41:42 -0400

    Create s1.java

---
+++ 16. 3Sum Closest/s1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        int result = 0;
+        int mingap = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length - 2; ++i){
+            int j = i + 1;
+            int k = nums.length - 1;
+            
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                int gap = Math.abs(target - sum);
+                if(gap < mingap){
+                    result = sum;
+                    mingap = gap;
+                }
+                
+                if(sum < target) ++j;
+                else
+                    --k;
+            }
+        }
+        return result;
+    }
+}
+// O(n^2)
+// O(1)
