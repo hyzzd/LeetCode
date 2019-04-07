d5e566b7885c4fea4776791b30f539f870358730
hyzzd <zhaoxiaohu920922@gmail.com>
2019-04-07 14:35:44 -0700

    Playground

--- Playground.java
+++ Playground.java
@@ -1,26 +1,12 @@
 import java.util.*;
 
 class Main {
-    public static void solution(int n) {
-        boolean[] primes = new boolean[n + 1];
-        Arrays.fill(primes, true);
-        for (int i = 2; i * i <= n; i++) {
-            if (primes[i]) {
-                for (int j = i * i; j <= n; j += i) {
-                    primes[j] = false;
-                }
-            }
-        }
-
-        for (int i = 2; i <= n; i++) {
-            if (primes[i]) {
-                System.out.println(i);
-            }
-        }
+    public static void solution() {
+        
     }
 
     public static void main(String[] args) {
-        solution(100);
+        solution();
         System.out.println("Hello World");
     }
 }
