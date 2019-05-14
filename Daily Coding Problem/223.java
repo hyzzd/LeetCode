fc11d2de8d057ef5150f758baab78b64482cf510
hyzzd <zhaoxiaohu920922@gmail.com>
2019-05-14 02:16:37 -0700

    223

--- Daily Coding Problem/223.java
+++ Daily Coding Problem/223.java
@@ -9,9 +9,7 @@ class Main {
         }
     }
 
-    public static void morrisTraversal(Node root) {
-        if (root == null)
-            return;
+    public static void morrisTraversalInOrder(Node root) {
         Node pre = null;
         Node curr = root;
         while (curr != null) {
@@ -35,12 +33,94 @@ class Main {
         }
     }
 
+    public static void morrisTraversalPreOrder(Node root) {
+        Node pre = null;
+        Node curr = root;
+        while (curr != null) {
+            if (curr.left == null) {
+                System.out.println(curr.val);
+                curr = curr.right;
+            } else {
+                pre = curr.left;
+                while (pre.right != null && pre.right != curr) {
+                    pre = pre.right;
+                }
+                if (pre.right == null) {
+                    System.out.println(curr.val);
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    pre.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
+    }
+
+    public static void morrisTraversalPostOrder(Node root) {
+        Node dump = new Node(-1);
+        dump.left = root;
+        Node pre = null;
+        Node curr = dump;
+        while (curr != null) {
+            if (curr.left == null) {
+                curr = curr.right;
+            } else {
+                pre = curr.left;
+                while (pre.right != null && pre.right != curr) {
+                    pre = pre.right;
+                }
+                if (pre.right == null) {
+                    pre.right = curr;
+                    curr = curr.left;
+                } else {
+                    printReverse(curr.left, pre);
+                    pre.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
+    }
+
+    private static void printReverse(Node from, Node to) {
+        reverse(from, to);
+        Node p = to;
+        while (true) {
+            System.out.println(p.val);
+            if (p == from)
+                break;
+            p = p.right;
+        }
+        reverse(to, from);
+    }
+
+    private static void reverse(Node from, Node to) {
+        if (from == to)
+            return;
+        Node x = from;
+        Node y = from.right;
+        Node z = null;
+        while (true) {
+            z = y.right;
+            y.right = x;
+            x = y;
+            y = z;
+            if (x == to)
+                break;
+        }
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        morrisTraversal(root);
+        morrisTraversalInOrder(root);
+        System.out.println();
+        morrisTraversalPreOrder(root);
+        System.out.println();
+        morrisTraversalPostOrder(root);
+        System.out.println();
     }
 }
