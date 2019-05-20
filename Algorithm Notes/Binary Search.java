68047e1755e604e7841b70fec5712f802757b7e4
hyzzd <zhaoxiaohu920922@gmail.com>
2019-05-20 10:33:27 -0700

    Binary Search

--- Algorithm Notes/Binary Search.java
+++ Algorithm Notes/Binary Search.java
@@ -1,15 +1,41 @@
 class BinarySearch {
-    public int binarySearch(int[] arr, int l, int r, int val) {
+    public int binarySearch(int[] a, int l, int r, int val) {
         while (l < r) {
             int m = l + (r - l) / 2;
-            if (arr[m] == val)
+            if (a[m] == val)
                 return m;
-            if (arr[m] > val) {
+            if (a[m] > val) {
                 r = m;
             } else {
                 l = m + 1;
             }
         }
-        return -1;
+        return l; // -1
+    }
+
+    public int lowerBound(int[] a, int l, int r, int val) {
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            // first index of i such that a[i] >= x
+            if (a[m] >= val) { // g(m)
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+
+    public int upperBound(int[] a, int l, int r, int val) {
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            // first index of i such that a[i] > x
+            if (A[m] > val) { // g(m)
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
     }
 }
