732fd9cb401226cab1012ebc1b990c65587a0a39
hyzzd <zhaoxiaohu920922@gmail.com>
2019-06-17 03:23:41 -0700

    258

---
+++ Daily Coding Problem/258.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Main {
+    class Node {
+        int val;
+        Node left, right;
+    }
+
+    public static List<Integer> bfs(Node root) {
+        List<Integer> res = new LinkedList<>();
+
+        if (root == null)
+            return res;
+
+        Queue<Node> q = new LinkedList<>();
+        q.offer(root);
+        boolean flag = false;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                Node curr = q.poll();
+                if (flag) {
+                    list.add(0, curr.val);
+                } else {
+                    list.add(curr.val);
+                }
+
+                if (curr.left != null)
+                    q.offer(curr.left);
+                if (curr.right != null)
+                    q.offer(curr.right);
+            }
+        }
+
+        return res;
+    }
+}
