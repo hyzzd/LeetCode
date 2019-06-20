dd528e1ba41618a01e71bccd5bb5483eaeaf8066
hyzzd <zhaoxiaohu920922@gmail.com>
2019-06-20 07:05:39 -0700

    260

--- Daily Coding Problem/260.java
+++ Daily Coding Problem/260.java
@@ -1,17 +1,27 @@
-/*
-def deduce_nums(arr):
-    ln = len(arr)
-    count_gt = sum([1 for x in arr if x == '+'])
-    first = ln - count_gt - 1
-    nums = [first]
-    small, large = first - 1, first + 1
-    for sym in arr[1:]:
-        if sym == '+':
-            nums.append(large)
-            large += 1
-        else:
-            nums.append(small)
-            small -= 1
-    
-    return nums
-*/
+class Main {
+    public int[] decode(int[] arr) {
+        int n = arr.length;
+        int[] nums = new int[n];
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == '+')
+                count++;
+        }
+
+        int first = n - count - 1;
+        nums[0] = first;
+        int small = first - 1, large = first + 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == '+') {
+                nums[i] = large;
+                large++;
+            } else {
+                nums[i] = small;
+                small--;
+            }
+        }
+
+        return nums;
+    }
+}
