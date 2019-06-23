5bb68b31af2d77ef054697dcbf63f2e99a1aec7a
hyzzd <zhaoxiaohu920922@gmail.com>
2019-06-23 07:53:04 -0700

    264

---
+++ Daily Coding Problem/264.java
@@ -0,0 +1,19 @@
+class DeBruijn {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+
+        String deBruijn = "";
+        for (int i = 0; i < n; i++)
+            deBruijn = deBruijn + "0";
+
+        for (int i = n; i < (1 << n); i++) {
+            String suffix = deBruijn.substring(i - n + 1);
+            if (deBruijn.indexOf(suffix + "1") == -1)
+                deBruijn = deBruijn + "1";
+            else
+                deBruijn = deBruijn + "0";
+        }
+        StdOut.println(deBruijn);
+
+    }
+}
