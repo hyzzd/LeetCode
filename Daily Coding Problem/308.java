02565d211aab8d9a741f39a78a5d57b4ee5c2db2
hyzzd <zhaoxiaohu920922@gmail.com>
2019-08-07 07:05:43 -0700

    308

---
+++ Daily Coding Problem/308.java
@@ -0,0 +1,15 @@
+// KAP_CONST = 6174
+
+
+// def apply_kproc(num, steps=0):
+//     if num == KAP_CONST:
+//         return steps
+
+//     digits = str(num)
+//     assert len(set(digits)) > 2
+
+//     asc_num = "".join(sorted(digits))
+//     dsc_num = "".join(sorted(digits, reverse=True))
+
+//     diff = int(dsc_num) - int(asc_num)
+//     return apply_kproc(diff, steps + 1)
