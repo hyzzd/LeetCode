ff9739d2c8e009b89a5d5aa11d8633fbc21e4b6b
Xiaohu Zhao <zhaoxiaohu920922@gmail.com>
2019-08-09 08:10:26 -0700

    311

---
+++ Daily Coding Problem/311.java
@@ -0,0 +1,14 @@
+// def find_peak(arr):
+//     if not arr:
+//         return None
+
+//     mid = len(arr) // 2
+
+//     if mid > 0 and arr[mid] > arr[mid - 1] and \
+//             mid < len(arr) and arr[mid] > arr[mid + 1]:
+//         return arr[mid]
+
+//     if mid > 0 and arr[mid] > arr[mid - 1]:
+//         return find_peak(arr[:mid])
+
+//     return find_peak(arr[mid + 1:])
