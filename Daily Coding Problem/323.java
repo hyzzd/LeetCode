04aa5312ba01da01ccf411339426e1e6999f0e7f
hyzzd <zhaoxiaohu920922@gmail.com>
2019-08-21 07:23:47 -0700

    323

---
+++ Daily Coding Problem/323.java
@@ -0,0 +1,14 @@
+// from heapq import heappush
+// from random import randint
+
+
+// def approx_median(arr):
+//     if not arr:
+//         return None
+
+//     sarr = list()
+//     for _ in range(len(arr)//2):
+//         rand = randint(0, len(arr) - 1)
+//     heappush(sarr, arr[rand])
+
+//     return sarr[len(sarr)//2]
