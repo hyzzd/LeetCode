9e5022977ee9be955ea74b8d1926120601129ebd
hyzzd <zhaoxiaohu920922@gmail.com>
2019-08-30 08:04:23 -0700

    332

---
+++ Daily Coding Problem/332.java
@@ -0,0 +1,8 @@
+// def get_variables(m, n):
+//     candidates = list()
+//     for a in range(m // 2 + 1):
+//         b = m - a
+//         if a ^ b == n:
+//             candidates.append((a, b))
+
+//     return candidates
