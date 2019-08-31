2a88dcf6fbd3c435bc91266e5219e5258f62985b
hyzzd <zhaoxiaohu920922@gmail.com>
2019-08-31 08:21:28 -0700

    342

---
+++ Daily Coding Problem/342.java
@@ -0,0 +1,21 @@
+// def reduce(lst, f, init):
+//     res = init
+//     for x in lst:
+//         res = f(res, x)
+//     return res
+
+
+// def add(a, b):
+//     return a + b
+
+
+// def multiply(a, b):
+//     return a * b
+
+
+// def custom_sum(lst):
+//     return reduce(lst, add, 0)
+
+
+// def custom_prod(lst):
+//     return reduce(lst, multiply, 1)
